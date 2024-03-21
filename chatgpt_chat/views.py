38c6a666d5fb7ad3ad6d557f7580523845ad0032
AllenLuo <1755838904@qq.com>
2024-03-21 19:43:16 +0800

    优化Claude模型逻辑

--- chatgpt_chat/views.py
+++ chatgpt_chat/views.py
@@ -42,6 +42,7 @@ class Chat(CustomModelViewSet):
           openai_chat_api_3_5_config_dict.update({i.key: i.value})
         logger.info(serializer.data)
         prompt = request.data["prompt"]
+        chat_serializer = ChatMessageSerializers(data=request.data)
         if serializer.data['chatModel'].startswith('gpt'):
           # chatgpt 处理逻辑
           if serializer.data.get('secretKey', 'None'):
@@ -61,10 +62,9 @@ class Chat(CustomModelViewSet):
             openai.api_base = openai_chat_api_3_5_config_dict.get("OPENAI_API_BASE_URL", 'None')
             openai_model = openai_chat_api_3_5_config_dict.get("model", 'None')
 
-          serializer = ChatMessageSerializers(data=request.data)
 
-          if serializer.is_valid(raise_exception=True):
-            logger.info(serializer.validated_data)
+          if chat_serializer.is_valid(raise_exception=True):
+            logger.info(chat_serializer.validated_data)
           try:
               completion = openai.ChatCompletion.create(
                   model= openai_model,
@@ -79,11 +79,13 @@ class Chat(CustomModelViewSet):
               prompt_tokens = completion.usage.prompt_tokens
               completion_tokens = completion.usage.completion_tokens
               total_tokens = completion.usage.total_tokens
-              serializer.save(prompt=prompt,baseUserId=request.user.id,completion=completion,chat_model=openai_model,
+              chat_serializer.save(prompt=prompt,baseUserId=request.user.id,completion=completion,chat_model=openai_model,
                   prompt_tokens=prompt_tokens,completion_tokens=completion_tokens,total_tokens=total_tokens)
 
               res = UserBenefits.objects.filter(baseUserId=baseUserId).first() # 更新用户福利表
               res.left_tokens -=total_tokens
+              if res.left_tokens < 0:
+                res.left_tokens = 0
               res.save()
 
               def generate_streaming_text(text=chat_text):
@@ -108,11 +110,33 @@ class Chat(CustomModelViewSet):
               logger.error(f'{msg}-后台返回-{e}')
               return ErrorResponse(code=500, data={}, msg=msg)
           
-        elif serializer.data['chatModel'].startswith('claude'):                  
+        elif serializer.data['chatModel'].startswith('claude'):
+          if chat_serializer.is_valid(raise_exception=True):
+            logger.info(chat_serializer.validated_data)
+          if serializer.data.get('secretKey', 'None'):
+            # 自定义代理处理逻辑
+            openai.api_key = serializer.data.get('secretKey', 'None')
+            openai.api_base = serializer.data.get('proxyAdress', 'None')
+            openai_model = serializer.data.get('chatModel', 'None')
+
+          elif res.VIP_TYPE == 1:
+            # 会员处理逻辑
+            openai.api_key = openai_chat_api_3_5_config_dict.get("OPENAI_API_KEY", 'None')
+            openai.api_base = openai_chat_api_3_5_config_dict.get("OPENAI_API_BASE_URL", 'None')
+            openai_model = serializer.data.get('chatModel', 'None')
+          
+          else:
+            openai.api_key = openai_chat_api_3_5_config_dict.get("OPENAI_API_KEY", 'None')
+            openai.api_base = openai_chat_api_3_5_config_dict.get("OPENAI_API_BASE_URL", 'None')
+            openai_model = openai_chat_api_3_5_config_dict.get("model", 'None')    
+
+          claude_config = Config.objects.filter(key='claude_base_url').first()
+
+
           # claude 处理逻辑
           client = Anthropic(
               api_key = openai_chat_api_3_5_config_dict.get("OPENAI_API_KEY", 'None'),
-              base_url = 'https://api.openai-proxy.org/anthropic'
+              base_url = claude_config.value
           )
 
           message = client.messages.create(
@@ -125,7 +149,20 @@ class Chat(CustomModelViewSet):
               ],
               model= serializer.data.get('chatModel', 'None'),
           )
-          logger.info(message.content[0].text)
+          logger.info(message)
+
+          prompt_tokens = message.usage.input_tokens
+          completion_tokens = message.usage.output_tokens
+          total_tokens = int(prompt_tokens) + int(completion_tokens)
+          chat_serializer.save(prompt=prompt,baseUserId=request.user.id,completion=message,chat_model=openai_model,
+              prompt_tokens=prompt_tokens,completion_tokens=completion_tokens,total_tokens=total_tokens)
+
+          res = UserBenefits.objects.filter(baseUserId=baseUserId).first() # 更新用户福利表
+          res.left_tokens -=total_tokens
+          if res.left_tokens < 0:
+             res.left_tokens = 0
+          res.save()
+
           def generate_streaming_text(text=message.content[0].text):
                   # 流媒体文本处理方法
                   id = str(uuid.uuid4())
